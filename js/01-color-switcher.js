c4e744cded3c99fb82c2fcb022f7ac4970587714
piotr.kostrzewa <pkostrzewa@gmail.com>
2024-01-05 20:57:40 +0100

    Homework - complete

--- js/01-color-switcher.js
+++ js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
+startButton.addEventListener('click', startColorChange);
+stopButton.addEventListener('click', stopColorChange);
 
+let intervalId;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function startColorChange() {
+  startButton.disabled = true;
+  stopButton.disabled = false;
+  intervalId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function stopColorChange() {
+  startButton.disabled = false;
+  stopButton.disabled = true;
+  clearInterval(intervalId);
+}
